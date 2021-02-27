96324bb535d5b9b2a55a239344c9545ac4b8d1f8
masterbogdan <maiskiychavk@gmail.com>
2021-02-27 20:23:38 +0200

    [finish basic REST API operations]

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,23 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { Character } from '../models/Character';
 
 const router = express.Router();
 
-router.get('/api', async (req: express.Request, res: express.Response) => {
+router.get('/api', async (req: Request, res: Response) => {
   try {
     const characters = await Character.find();
     if (characters) {
-      res.status(200);
-      res.json(characters);
+      res.status(200).json(characters);
     } else {
-      res.status(404);
+      res.status(404).json({ response: 'not found' });
     }
   } catch (error) {
     console.log(error);
+    res.status(500).json({ response: error });
   }
 });
 
-router.post('/api', async (req: express.Request, res: express.Response) => {
+router.post('/api', async (req: Request, res: Response) => {
   try {
     const { char } = req.body;
     if (char !== null && char !== undefined) {
@@ -27,14 +27,46 @@ router.post('/api', async (req: express.Request, res: express.Response) => {
         description: char.description,
       });
       await character.save();
-      res.status(200);
-      res.json({ message: 'success' });
+      res.status(200).json({ response: 'success' });
     } else {
       res.status(404);
-      res.json({ message: 'error' });
+      res.json({ response: 'error' });
     }
   } catch (error) {
     console.log(error);
+    res.status(500).json({ response: error });
+  }
+});
+
+router.put('/api', async (req: Request, res: Response) => {
+  try {
+    const { id, char } = req.body;
+    await Character.findByIdAndUpdate(id, { ...char }, null, (error) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ response: error });
+      }
+      res.status(200).json({ response: 'success' });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ response: error });
+  }
+});
+
+router.delete('/api', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.body;
+    await Character.findByIdAndDelete(id, null, (error) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ response: error });
+      }
+      res.status(200).json({ response: 'success' });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ response: error });
   }
 });
 
